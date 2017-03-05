70c707cd5ec333a45a4a2ebebd0558e18fba9b9c
reuben453 <reuben453@gmail.com>
2017-03-05 16:21:37 +0530

    Allow graphiql in production

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
 
   post '/graphql', to: 'graphql#query'
-  if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
 
 end
